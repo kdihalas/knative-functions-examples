ded42129a551aa2ab513426ff634e9850e7220d0
Kostas Dichalas <kdihalas@gmail.com>
2020-01-19 23:31:25 +0000

    first commit with nodejs builder

---
+++ nodejs/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
+
+const app = express();
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.json({
+    uuidv1: uuidv1(),
+    uuidv3: uuidv4(),
+  });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('uuid-generator listening on port', port);
+});
