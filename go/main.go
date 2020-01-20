048cf57d1fda3322bec0ec989fe7059d227386a4
Kostas Dichalas <kdihalas@gmail.com>
2020-01-20 02:29:56 +0000

    added more languages

---
+++ go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+  "github.com/satori/go.uuid"
+  "fmt"
+)
+
+func main() {
+  r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+    u1 := uuid.NewV1()
+    u4 := uuid.NewV4()
+    
+		c.JSON(200, gin.H{
+			"uuidv1": fmt.Sprintf("%s", u1),
+      "uuidv4": fmt.Sprintf("%s", u4),
+		})
+	})
+	r.Run()
+}
